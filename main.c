9801dd6c0830d28fd3bced2f68df9cdfe93d5e09
shyam.exe <shyamshivajij07@gmail.com>
2026-03-31 13:44:39 +0530

    1

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+//Bin Constants
 #define MAX_BINS 100
+#define BINS_CSV "bins.csv"
+//Waste Types
+#define DRY 
 
 struct Bin{
     int bin_id;
     int zone;
     int waste_type;
     float fill_level;
+    float capacity;
+    
 };
